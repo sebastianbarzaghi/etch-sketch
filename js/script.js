c0decccd77350e6a748368af4e8546c18a33fb4c
sebastianbarzaghi <sebastian.barzaghi@outlook.com>
2023-04-01 15:58:08 +0200

    fix: added randomizer logic on eraser branch

--- js/script.js
+++ js/script.js
@@ -16,15 +16,14 @@ for (let i = 0; i < 16; i++) {
 const gridCells = document.querySelectorAll(".cell");
 const colorPicker = document.querySelector(".colorPicker");
 
-let drawActive = false;
-let eraserActive = false;
+//let eraserActive = false;
+//let erasing = false;
 
-//eraser
+/*
 const eraserBtn = document.querySelector(".eraserBtn");
 eraserBtn.addEventListener("click", function() {
     if (eraserActive) {
         eraserActive = false;
-        drawActive = true;
         eraserBtn.classList.remove("clicked");
     } else {
         eraserActive = true;
@@ -33,37 +32,84 @@ eraserBtn.addEventListener("click", function() {
     };
 });
 
-// draw or erase
+const funcButtons = document.querySelectorAll(".addFunc");
+funcButtons.forEach(function(btn) {
+    let isClicked = false;
+    btn.addEventListener("click", function() {
+        if (!isClicked) {
+            isClicked = true;
+            btn.classList.add("clicked");
+        } else {
+            isClicked = false;
+            btn.classList.remove("clicked");
+        }
+    })
+})
+*/
+
+// draw
+let drawActive = false;
 gridCells.forEach(function(cell) {
     cell.addEventListener("mousedown", function() {
         if (!drawActive) {
             drawActive = true;
-            //color picker
-            cell.style.backgroundColor = colorPicker.value;
-        } else {
-            if (eraserActive) {
-                cell.style.removeProperty("background-color");
-            }
+            if (randomActive) {
+                cell.style.backgroundColor = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+            } else {
+                //color picker
+                cell.style.backgroundColor = colorPicker.value;
+            };
         };
     });
     cell.addEventListener("mousemove", function() {
         if (drawActive) {
-            //color picker
-            cell.style.backgroundColor = colorPicker.value;
-        } else {
-            if (eraserActive) {
-                cell.style.removeProperty("background-color");
-            }
+            if (randomActive) {
+                cell.style.backgroundColor = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+            } else {
+                //color picker
+                cell.style.backgroundColor = colorPicker.value;
+            };
         };
     });
     cell.addEventListener("mouseup", function() {
-        if(drawActive) {
+        if (drawActive) {
             drawActive = false;
         };
     });
-})
+});
 
+// random
+let randomActive = false;
+const randomBtn = document.querySelector(".randomBtn");
+randomBtn.addEventListener("click", function() {
+    if (!randomActive) {
+        randomActive = true;
+        randomBtn.classList.add("clicked");
+    } else {
+        randomActive = false;
+        randomBtn.classList.remove("clicked");
+    };
+});
 
+//eraser
+//shading
+//lighten
+//toggle grid
+
+/* grabber
+let grabberActive = false;
+const grabberBtn = document.querySelector(".grabberBtn");
+grabberBtn.addEventListener("click", function() {
+    if (!grabberActive) {
+        grabberActive = true;
+        drawActive = false;
+        grabberBtn.classList.add("clicked");
+    } else {
+        grabberActive = false;
+        grabberBtn.classList.remove("clicked");
+    };
+});
+*/
 
 
 
